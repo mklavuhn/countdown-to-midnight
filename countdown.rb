c16a1c12dd5a3dd494c4b14c756914cf1bf82bbc
mklavuhn <michaelklavuhn3@gmail.com>
2021-07-27 11:52:26 -0700

    commit tree b95564996bc37daa8dfd6e08d8f62938bc70fc1d

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
 
-def countdown
+
+def countdown(n)
+    while n >= 1 
+        puts "#{n} SECOND(S)!"
+        n -= 1 
+        if n == 0 
+            return "HAPPY NEW YEAR!"
+        end
+    end
+end
+
+def countdown_with_sleep(n) 
+    sleep n
 end
